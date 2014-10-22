909c0ea28e3d2a4ff910aae2852968ad5f889fb0
shanson82 <steven.w.hanson@gmail.com>
2014-10-22 13:57:20 -0500

    code update

--- Database.java
+++ Database.java
@@ -1,5 +1,71 @@
 package org.uiowa.cs2820.engine;
 
-public class Database {
+import java.util.*;
 
+public class Database {
+	 
+	// create ArrayList to store information
+	private static ArrayList<String> storage = new ArrayList<String>();
+	
+	
+	public static ArrayList<String> getStorage() {
+		return storage;
+	}
+
+	public static void setStorage(ArrayList<String> storage) {
+		Database.storage = storage;
+	}
+
+	public static void insert(String identifier, String fieldName, String fieldValue) {
+		String record = identifier + " " + fieldName + " " + fieldValue;
+		// only add the record if it is not already contained in the list
+		if (Database.storage.contains(record) == false) {
+			Database.storage.add(record);
+		}
+	}
+	
+	public static String search(String fieldName, String fieldValue) {
+		//String identifiers = new String();
+		String identifiers = "";
+		for (String s : Database.storage) {
+			String[] recordArray = s.split("\\s+");
+			if (fieldName.equals(recordArray[1]) && fieldValue.equals(recordArray[2])) {
+				System.out.println("in initial if");
+				if (identifiers == "") {
+					
+					identifiers = recordArray[0];
+				} else {
+					identifiers = " " + recordArray[0];
+				}		
+			}
+		}
+		return identifiers;
+	}
+	
+	public static int size() {
+		return Database.storage.size();
+	}
+
+	public static String arrayListToString() {
+		String contents = new String();
+		for (String s : Database.storage) {
+			contents = contents + s;
+		}
+		return contents;
+		
+	}
+
+	
+/*	
+	public static void main(String[] args) {
+		Database.insert("doc1", "part", "nail");
+		Database.insert("doc1", "part", "nail");
+		Database.insert("doc2", "partNo", "1234");
+		System.out.println(Database.size());
+		//System.out.println(Database.toString());
+		System.out.println(Database.search("part", "nail"));
+		System.out.println(Database.search("partNo", "nail"));
+		System.out.println(Database.search("partNo", "1234"));
+	}
+*/
 }
--- DatabaseTest.java
+++ DatabaseTest.java
@@ -5,10 +5,49 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class DatabaseTest {
-
+	/*
 	@Test
 	public void test() {
 		fail("Not yet implemented");
 	}
-
+	*/
+	// insert one Field and identifier and check 
+	@Test
+	public void test1() {
+		Database.insert("doc1", "part", "nail");
+		assertEquals(Database.arrayListToString(), "doc1 part nail");
+	}
+	
+	// insert multiple of the same Field and identifier, make sure it only appears once
+	@Test
+	public void test2() {
+		Database.insert("doc1", "part", "nail");
+		Database.insert("doc1", "part", "nail");
+		assertEquals(Database.size(), 1);		
+	}
+	
+	// insert one Field and identifier
+	// perform search and assert that returned identifier matches inputted identifier
+	@Test
+	public void test3() {
+		Database db = new Database();
+		
+	}
+	
+	// insert multiple of same Field with same identifier
+	// search and assert that only one identifier is returned
+	@Test
+	public void test4() {
+		Database db = new Database();
+		
+	}
+	
+	// insert multiple of same Field with different identifiers
+	// search for Field and assert inputted identifiers match returned identifiers
+	@Test
+	public void test5() {
+		Database db = new Database();
+		
+	}
+	
 }
