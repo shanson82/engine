2d1763f1d48dae7ef2c8975dff8f09d5e833291c
Clement Yan <clement-yan@uiowa.edu>
2014-10-23 16:39:58 -0500

    Added Indexer.isClosed() method
    Added Indexer.isClosed() method

--- IndexerTest.java
+++ IndexerTest.java
@@ -1,27 +1,53 @@
-package org.uiowa.cs2820;
+package org.uiowa.cs2820.engine;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
 
 public class IndexerTest {
+	public Indexer I;
 	public IndexerTest(){}
+	
+	@Before
+	public void before(){
+		I = new Indexer("someIdentifier");
+	}
+	
+	@After
+	public void after(){
+		I = null;
+	}
 
 	@Test
 	public void testAdd(){
-		Indexer I = new Indexer("someIdentifier");
 		try{I.add(new Field("someFieldName", "someFieldValue"));}
 		catch(Exception e){fail("Exception thrown by add()");}
 	}
 	
 	@Test
 	public void testClose(){
-		Indexer I = new Indexer("someIdentifier");
 		try{
 			I.close();
 			I.add(new Field("someFieldName", "someFieldValue"));
 			fail("Successfully written to closed Indexer");
 		}catch(Exception e){}
+	}
+	
+	@Test
+	public void testIsClosed1(){
+		try{I.add(new Field("someFieldName", "someFieldValue"));}
+		catch(Exception e){fail("Exception thrown by add()");}
+		assertFalse("isClosed() returned true before close",I.isClosed());
+	}
+	
+	@Test
+	public void testIsClosed2(){
+		try{I.add(new Field("someFieldName", "someFieldValue"));}
+		catch(Exception e){fail("Exception thrown by add()");}
+		I.close();
+		assertTrue("isClosed() returned false after close",I.isClosed());
 	}
 
 }
