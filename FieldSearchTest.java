dc5329ad4cdd9aabe9bee8e214c7209c09b4d852
shanson82 <steven.w.hanson@gmail.com>
2014-10-23 16:18:55 -0500

    adding FieldSearchTest

---
+++ FieldSearchTest.java
@@ -0,0 +1,48 @@
+package org.uiowa.cs2820.engine;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FieldSearchTest {
+
+	@Test
+	public void test1() {
+		FieldSearch T = new FieldSearch();
+		String[] result = {"doc1", "doc2"};
+		Database.insert("doc1", "part", "nail");
+		Database.insert("doc2", "part", "nail");
+		assertArrayEquals(T.findEquals("part","nail"), result);
+	}
+
+	@Test
+	public void test2() {
+		FieldSearch T = new FieldSearch();
+		String[] result = {"doc1", "doc2", "doc3"};
+		Database.insert("doc1", "part", "nail");
+		Database.insert("doc2", "part", "nail");
+		Database.insert("doc3", "part", "nail");
+		assertArrayEquals(T.findEquals("part","nail"), result);
+	}
+
+
+	@Test
+	public void test3() {
+		FieldSearch T = new FieldSearch();
+		String[] result = {"doc1"};
+		Database.insert("doc1", "hammer", "nail");
+		Database.insert("doc2", "hammer", "nail");
+		Database.insert("doc3", "hammer", "nail");
+		assertArrayEquals(T.findEquals("part","screwdrivers"), result);
+	}
+	
+	@Test
+	public void test4() {
+		FieldSearch T = new FieldSearch();
+		String[] result = {""};
+		Database.insert("doc1", "part", "nail");
+		Database.insert("doc2", "part", "nail");
+		Database.insert("doc3", "part", "nail");
+		assertArrayEquals(T.findEquals("no","result"), result);
+	}
+}
