045592f58187a330c1abecb870a2d5f6248d144d
shanson82 <steven.w.hanson@gmail.com>
2014-10-24 16:17:45 -0500

    minor change to FieldSearchTest

--- FieldSearchTest.java
+++ FieldSearchTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class FieldSearchTest {
 
 	@Test
-	public void test1() {
+	public void test1() throws Exception {
 		FieldSearch T = new FieldSearch();
 		String[] result = {"doc1", "doc2"};
 		Database.insert("doc1", "part", "nail");
@@ -16,7 +16,7 @@ public class FieldSearchTest {
 	}
 
 	@Test
-	public void test2() {
+	public void test2() throws Exception {
 		FieldSearch T = new FieldSearch();
 		String[] result = {"doc1", "doc2", "doc3"};
 		Database.insert("doc1", "part", "nail");
@@ -27,17 +27,17 @@ public class FieldSearchTest {
 
 
 	@Test
-	public void test3() {
+	public void test3() throws Exception {
 		FieldSearch T = new FieldSearch();
 		String[] result = {"doc1"};
-		Database.insert("doc1", "hammer", "nail");
+		Database.insert("doc1", "part", "screwdrivers");
 		Database.insert("doc2", "hammer", "nail");
 		Database.insert("doc3", "hammer", "nail");
 		assertArrayEquals(T.findEquals(new Field("part","screwdrivers")), result);
 	}
 	
 	@Test
-	public void test4() {
+	public void test4() throws Exception {
 		FieldSearch T = new FieldSearch();
 		String[] result = {""};
 		Database.insert("doc1", "part", "nail");
